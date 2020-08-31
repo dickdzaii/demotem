2a95563b791df5ca8fe5b245b3ae596a0ed9958c
dickdzaii <70245065+dickdzaii@users.noreply.github.com>
2020-08-31 10:24:04 +0700

    update

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,7 +9,7 @@ export class MenuComponent implements OnInit {
   menus =[{name:'Trang chủ',url:'',disabled:false,child:null}, {name:'Các hãng sản xuất',disabled:false,url:'#',child:
   [{name:'Apple',url:'#'},{name:'SamSung',url:'#'},{name:'-',url:''},{name:'Vsmart',url:'#'},
   {name:'Bphone',url:'#'}]}, {name:'Tin tức',url:'',disabled:true,child:null},{name:'Trợ giúp',url:'',disabled:true,child:null}
-] ;
+] ;//toidachinhsua
   constructor() { }
 
   ngOnInit(): void {
